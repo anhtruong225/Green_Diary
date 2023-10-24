867fd6ae9225b1c6ee6d0e77ecc1c2b9bd95c430
Ajla Saranovic <ge43xed@mytum.de>
2023-10-24 09:28:39 +0200

    plants code

--- server/index.js
+++ server/index.js
@@ -2,14 +2,20 @@ import express from "express";
 import "./db/server.js";
 import mongoose from "mongoose";
 import plantsRouter from "./routes/plantsRouter.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(cors());
+app.use(express.json());
 app.use("/plants", plantsRouter);
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
 app.listen(port, () => {
   console.log(`The app listening on port ${port}`);
 });
